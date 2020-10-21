c9b09dcd09bdcb60d49733e9890b94eeafaa29fe
ocassional <73037761+ocassional@users.noreply.github.com>
2020-10-21 14:21:17 +0100

    Update filePosix.c
    Modified the code to read in bulk instead of reading byte by byte.
    Error handling in place.

--- filePosix.c
+++ filePosix.c
@@ -2,25 +2,40 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+#define BUF_SIZE 1024
 
 int main(int argc, char **argv)
 {
     int fd;
-    char ch;
+    char ch[BUF_SIZE];
 
     if (2 != argc)
     {
-        fprintf(stderr, "usage: %s <output_file>\n", argv[0]);
+        fprintf(stderr, "Error[%s, line: %d]: Usage: %s <output_file>\n", __FILE__, __LINE__, argv[0]);
         exit(1);
     }
     if ((fd = open(argv[1], O_CREAT|O_TRUNC|O_WRONLY, 0644)) < 0) 
     {
-        fprintf(stderr, "failed to open file : %s\n", argv[1]);
+        fprintf(stderr, "Error[%s, line: %d]: Failed to open file : %s\n", __FILE__, __LINE__, argv[1]);
         exit(1);
     }
-    while(read(STDIN_FILENO, &ch, 1) > 0)
+    ssize_t bytes_read;
+    while( (bytes_read = read(STDIN_FILENO, &ch, sizeof(ch))) > 0)
     {
-        write(fd, &ch, sizeof(ch));
+        ssize_t retVal = write(fd, &ch, bytes_read);
+        if(retVal != bytes_read)
+        {
+            fprintf(stderr, "Error[%s, line: %d]: Errno <%d>: %s\n", __FILE__, __LINE__, errno, strerror(errno));
+            exit(1);
+        }
+    }
+
+    if(bytes_read < 0)
+    {
+        fprintf(stderr, "Error[%s, line: %d]: Errno <%d>: %s\n", __FILE__, __LINE__, errno, strerror(errno));
     }
     close(fd);
 
