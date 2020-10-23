c95b2ac5b036391a7fd7463a7715af69e6b9b0a0
ocassional <73037761+ocassional@users.noreply.github.com>
2020-10-23 18:26:34 +0100

    Update filePosix.c
    - separated the file creation logic from main.
    - improved the error handling.

--- filePosix.c
+++ filePosix.c
@@ -6,38 +6,56 @@
 #include <errno.h>
 
 #define BUF_SIZE 1024
-
-int main(int argc, char **argv)
+int createFile(const char* fileName)
 {
     int fd;
     char ch[BUF_SIZE];
 
-    if (2 != argc)
+    if(!fileName)
     {
-        fprintf(stderr, "Error[%s, line: %d]: Usage: %s <output_file>\n", __FILE__, __LINE__, argv[0]);
-        exit(1);
+        fprintf(stderr, "NULL fileName passed\n");
+        return -1;
     }
-    if ((fd = open(argv[1], O_CREAT|O_TRUNC|O_WRONLY, 0644)) < 0) 
+
+    if ((fd = open(fileName, O_CREAT|O_TRUNC|O_WRONLY, 0644)) < 0)
     {
-        fprintf(stderr, "Error[%s, line: %d]: Failed to open file : %s\n", __FILE__, __LINE__, argv[1]);
-        exit(1);
+        fprintf(stderr, "Error[%s, line: %d]: Failed to open file : %s\n", __FILE__, __LINE__, fileName);
+        return -1;
     }
     ssize_t bytes_read;
     while( (bytes_read = read(STDIN_FILENO, &ch, sizeof(ch))) > 0)
     {
         ssize_t retVal = write(fd, &ch, bytes_read);
+        
         if(retVal != bytes_read)
         {
-            fprintf(stderr, "Error[%s, line: %d]: Errno <%d>: %s\n", __FILE__, __LINE__, errno, strerror(errno));
-            exit(1);
+            fprintf(stderr, "Error[%s, line: %d]: write failed, Errno <%d>: %s\n", __FILE__, __LINE__, errno, strerror(errno));
+            close(fd);
+            return -1;
         }
     }
 
     if(bytes_read < 0)
     {
-        fprintf(stderr, "Error[%s, line: %d]: Errno <%d>: %s\n", __FILE__, __LINE__, errno, strerror(errno));
+        fprintf(stderr, "Error[%s, line: %d]: read failed, Errno <%d>: %s\n", __FILE__, __LINE__, errno, strerror(errno));
+        close(fd);
+        return -1;
     }
-    close(fd);
+    return close(fd);
+}
 
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s <output_file>\n", argv[0]);
+        exit(1);
+    }
+    
+    // some validation around fileName can go here before passing it to the createFile
+    if (-1 == createFile(argv[1]))
+    {
+        fprintf(stderr, "File creation failed\n");
+    }
     return 0;
-} 
+}
